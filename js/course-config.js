b5e3138f8acf3a47dff885637afb1de613183237
paulina-01 <paufk2001@gmail.com>
2026-08-30 12:19:43 -0400

    staged the Unlock full course content to open to PWA, still with bugs

--- js/course-config.js
+++ js/course-config.js
@@ -3,7 +3,7 @@
  * public; never add Stripe secret keys or Supabase service-role keys here.
  */
 window.BTP_COURSE_CONFIG = {
-  supabaseUrl: 'https://paktsqnbtetsmupmmeit.supabase.co/rest/v1/',
+  supabaseUrl: 'https://paktsqnbtetsmupmmeit.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBha3RzcW5idGV0c211cG1tZWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwMTk4NTEsImV4cCI6MjEwMzU5NTg1MX0.SWucYJ7Pi5Y2zxwJkK7De_9aDCabmUzdYx3s3Ja7Kl0',
   courseProductKey: 'full-course'
 };
